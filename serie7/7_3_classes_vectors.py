245838be67473b0c99466d4d32c617e290fe6c2b
Robert C <robertryan.chavez@hotmail.com>
2020-04-30 17:38:06 +0200

    add ex7.3, ex7.4

---
+++ serie7/7_3_classes_vectors.py
@@ -0,0 +1,63 @@
+﻿class Vector:
+    def add(z1, z2):
+        """ Adds two vectors """
+        if type(z1) is not list or type(z2) is not list:
+            raise TypeError("Not a vector")
+        elif len(z1) != len(z2):
+           raise ValueError("Dimensions do not match")
+        return [a + b for a, b in zip(z1, z2)]
+
+    def scalar(a, z1):
+        """ Scalar-Vector multiplication """
+        if type(a) not in [int, float]:
+            raise TypeError("Scalar is not numerical")
+        elif type(z1) is not list:
+            raise TypeError("Not a vector")
+        return [a*z for z in z1]
+
+
+class VectorPlus(Vector):
+    def vector_prod(z1, z2):
+        """ Calculates and returns the vector product / cross product 
+            of two vectors. Only works for 3 dimensional vectors.
+        """
+        if type(z1) is not list or type(z2) is not list:
+            raise TypeError("Not a vector")
+        elif len(z1) != 3 or len(z2) != 3:
+            raise ValueError("Only three-dimensional vectors allowed")
+        return [z1[1]*z2[2] - z1[2]*z2[1],
+                z1[2]*z2[0] - z1[0]*z2[2],
+                z1[0]*z2[1] - z1[1]*z2[0]]
+
+    def tensor(z1, z2):
+        """ Calculates and returns the tensor product of two vectors 
+            Note: Forming the tensor product z1 x z2 is like forming 
+            the Cartesion product of two sets
+        """
+        if type(z1) is not list or type(z2) is not list:
+            raise TypeError("Not a vector")
+        newvector = []
+        for x in z1:
+            for y in z2:
+                newvector.append(x*y)
+        return newvector
+
+
+if __name__ == "__main__":
+    a = [1, 2, 3]
+    b = [5, 5, 6]
+    c = [4, 5]
+
+    # Test Addition
+    print(Vector.add(a, b))
+    print(VectorPlus.add(a, b))
+
+    # Test Scalar Multiplication
+    print(Vector.scalar(3, a))
+    print(VectorPlus.scalar(3, b))
+
+    # Test Vector product
+    print(VectorPlus.vector_prod(a, b))
+
+    # Test Tensor product
+    print(VectorPlus.tensor(a, c))
---
+++ serie7/7_4_classes_faculty.py
@@ -0,0 +1,60 @@
+﻿import math
+
+# (1) ... formula seen on exercise sheet
+
+class faculty:
+    def fac(n, ptr = False):
+        """ Return n! """
+        fac = math.factorial(n)
+        if ptr:
+            print(fac)
+        return fac
+
+    def fac_stir(n, ptr = False):
+        """ Using Stirling formula to approximate n! """
+        fac = (2*math.pi*n)**0.5 * (n/math.e)**n
+        if ptr:
+            print(fac)
+        return fac
+
+    def fac_gam(n, ptr = False):
+        """ Using (1) to approximate n! """
+        z = n + 1
+        fac = ((2*math.pi)/z)**0.5 * \
+              ((1/math.e)*(z + 1/(12*z - 1/(10*z))))**z
+        if ptr:
+            print(fac)
+        return fac
+
+    def fac_stir_err(n, ptr = False):
+        """ Return the relative error of the stirling formula """
+        err = abs(faculty.fac(n) - faculty.fac_stir(n))
+        if ptr:
+            print(err)
+        return err
+
+    def fac_gam_err(n, ptr = False):
+        """ Return the relative error of the (1) approximation """
+        err = abs(faculty.fac(n) - faculty.fac_gam(n))
+        if ptr:
+            print(err)
+        return err
+
+
+if __name__ == "__main__":
+    n = 10
+
+    # Test class methods
+    print("Exact n!:", faculty.fac(n))
+    print("Using Stirling:", faculty.fac_stir(n))
+    print("Using (1):", faculty.fac_gam(n))
+    print("Stirling Error:", faculty.fac_stir_err(n))
+    print("(1) Error:", faculty.fac_gam_err(n))
+
+    # Test print flags
+    print("===== Print flag outputs =====")
+    faculty.fac(n, True)
+    faculty.fac_stir(n, True)
+    faculty.fac_gam(n, True)
+    faculty.fac_stir_err(n, True)
+    faculty.fac_gam_err(n, True)
