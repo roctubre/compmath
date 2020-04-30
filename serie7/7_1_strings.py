26dc29503dd552c28f807827a633428552a83595
Robert C <robertryan.chavez@hotmail.com>
2020-04-30 03:44:16 +0200

    add ex7.1, ex7.2

---
+++ serie7/7_1_strings.py
@@ -0,0 +1,20 @@
+﻿
+# a) Uses string.replace()
+def find_replace(s_in, s_find, s_replace):
+    return s_in.replace(s_find, s_replace)
+
+# b) Uses string.upper(), string.split()
+def f(s, capitalize = False):
+    if capitalize:
+        return s.upper().split()
+    else:
+        return s.split()
+
+
+if __name__ == "__main__":
+    # test a)
+    print(find_replace("ded did dod, bla bli dod", "dod", "hunga"))
+
+    # test b)
+    print(f("Hello World! Mama is here"))
+    print(f("Hello World! Daddy is here", True))
---
+++ serie7/7_2_classes_quaternions.py
@@ -0,0 +1,53 @@
+﻿# Using definition from https://de.wikipedia.org/wiki/Quaternion
+
+class Quaternion:
+    def __init__(self, x0, x1, x2, x3):
+        self.x0 = x0
+        self.x1 = x1
+        self.x2 = x2
+        self.x3 = x3
+
+    def __add__(self, o):
+        """ Overloading + operator
+            Addition between two Quaternions
+        """
+        if type(o) is not Quaternion:
+            raise TypeError("Second Value is not a Quanterion")
+        return Quaternion(self.x0 + o.x0, self.x1 + o.x1, 
+                          self.x2 + o.x2, self.x3 + o.x3)
+
+    def __mul__(self, o):
+        """ Overloading * operator
+            Multiplication between two Quaternions
+        """
+        if type(o) is not Quaternion:
+            raise TypeError("Second Value is not a Quanterion")
+        return Quaternion(self.x0*o.x0 - self.x1*o.x1 - self.x2*o.x2 - self.x3*o.x3,
+                          self.x0*o.x1 + self.x1*o.x0 + self.x2*o.x3 - self.x3*o.x2,
+                          self.x0*o.x2 - self.x1*o.x3 + self.x2*o.x0 + self.x3*o.x1,
+                          self.x0*o.x3 + self.x1*o.x2 - self.x2*o.x1 + self.x3*o.x0)
+
+    def norm(self):
+        """ Returns the norm of the Quaternion """
+        return self.x0**2 + self.x1**2 + self.x2**2 + self.x3**2
+
+    def __str__(self):
+        """ Overload string output """
+        return "[" + ", ".join([str(self.x0), str(self.x1), str(self.x2), str(self.x3)]) + "]"
+        
+
+
+if __name__ == "__main__":
+    a = Quaternion(1, 2, 3, 4)
+    b = Quaternion(5, 5, 6, 6)
+
+    # Test Addition
+    print(a + b)
+
+    # Test Mulitplication: Quaternions are not commutative
+    print(a * b)
+    print(b * a)
+
+    # Test Norm
+    print(a.norm())
+    print(b.norm())
