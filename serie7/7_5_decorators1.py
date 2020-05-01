bbbcd50d852f28f9f9dc719037ca2f9541fb65dd
Robert C <robertryan.chavez@hotmail.com>
2020-05-02 00:24:55 +0200

    add ex7.5, ex7.6

---
+++ serie7/7_5_decorators1.py
@@ -0,0 +1,37 @@
+﻿import math
+
+# a)
+def dec(ev, fun):
+    """ Returns a function that evaluates with given parameters when called """
+    def wrapper():
+        return fun(ev)
+    return wrapper
+
+# b)
+def comp(l):
+    """ Returns a function which is the composition of functions
+        in a given list
+    """
+    def deco(x):
+        val = x
+        for f in reversed(l):
+            val = f(val)
+        return val
+
+    return deco
+
+if __name__ == "__main__":
+    # a) Test decorator
+    mydec1 = dec(math.pi/2, math.sin) # create decorator with sin()
+    mydec2 = dec(1, math.exp) # create decorator with exp()
+    print(mydec1) # address of function
+    print(mydec2) # address of function
+    print(mydec1()) # execute function; sin(pi/2) = 1
+    print(mydec2()) # execute function; e**1 = e = 2.71
+
+    # b) Test decorator - composition
+    l1 = [math.fabs, math.cos, math.asin, math.sin, math.atan, math.tan]
+    mycomp = comp(l1)
+    print(mycomp) # address of function
+    print(mycomp(math.pi)) # execute function -> 1
+    print(mycomp(0)) # execute function -> 1
---
+++ serie7/7_6_decorators2.py
@@ -0,0 +1,31 @@
+﻿import math
+
+
+def count(fun):
+    """ Returns a decorator function, which when called returns 
+        the call count and the evaluation 
+    """
+    counter = 0
+    def deco(x):
+        nonlocal counter
+        counter += 1
+        return counter, fun(x)
+
+    return deco
+
+
+if __name__ == "__main__":
+    # Declare two decorators
+    f = count(math.sin)
+    f2 = count(math.cos)
+
+    # Test first decorator
+    print(f(0.1))
+    print(f(0.2))
+
+    # Second decorator; doesn't affect count of first decorator
+    print(f2(0))
+    print(f2(math.pi))
+
+    # And first again
+    print(f(0.3))
