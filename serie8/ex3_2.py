d5213145c8be126549b2e3ee8c93ccc5529df480
Robert C <robertryan.chavez@hotmail.com>
2020-05-06 19:42:33 +0200

    add ex3.2, ex3.4

---
+++ serie8/ex3_2.py
@@ -0,0 +1,23 @@
+﻿import numpy as np
+
+
+def chessboard(n, display = True):
+    """
+    Creates a chessboard-matrix with dimensions n times n
+
+    To account for uneven matrices, first a 2n times 2n matrix is created, then
+    only the upper left part of the matrix (n times n) is sliced out
+    """
+    even_row = np.array([1,0]*n)
+    uneven_row = np.array([0,1]*n)
+    stacked = np.row_stack((even_row, uneven_row)*n)
+    matrix = stacked[:n,:n]
+
+    if display:
+        print(matrix)
+
+    return matrix
+
+
+if __name__ == "__main__":
+    chessboard(7)
---
+++ serie8/ex3_4.py
@@ -0,0 +1,25 @@
+﻿import numpy as np
+
+
+def diagonals(n, display = True):
+    """
+    Creates a matrix with ones on the diagonal and anti-diagonal, while all
+    other entries are zero.
+    """
+
+    # First create an id-matrix (diagonal) and its flipped version (anti-diagonal)
+    matrix = np.identity(n)
+    trans = np.flip(matrix, axis = 1)
+
+    # Fuse them together by masking
+    mask = (matrix == 0)
+    matrix[mask] = trans[mask]
+
+    if display:
+        print(matrix)
+
+    return matrix
+
+
+if __name__ == "__main__":
+    diagonals(5)
