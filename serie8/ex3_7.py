23ffb3e025ca0a1c22411173fce7234bb63e6e8b
Robert C <robertryan.chavez@hotmail.com>
2020-05-12 19:24:39 +0200

    add ex3.7

---
+++ serie8/ex3_7.py
@@ -0,0 +1,29 @@
+﻿import numpy as np
+from numpy.linalg import det
+
+def wedge(w_vecs):
+    """
+    Returns a function to calculate the wedge product.
+    Basis w-vectors must be given.
+    """
+    def prod(v_vecs):
+        """
+        Evaluates the wedge product given v-vectors.
+
+        It first performs the euclidean scalar product on the basis with each vector in v.
+        The resulting array is then used to get the determinant.
+        """
+        return det(np.array([[np.dot(w_vecs, v) for w_vecs in w_vecs] for v in v_vecs]))
+
+    return prod
+
+
+def main():
+    w_vecs = [[1,2,3],[1,1,1],[1,0,2]]
+    v_vecs = [[1,1,2],[1,1,1],[2,1,1]]
+    f = wedge(w_vecs)
+    print(f(v_vecs))
+
+
+if __name__ == "__main__":
+    main()
