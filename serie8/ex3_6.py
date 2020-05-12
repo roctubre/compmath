5b0b3952b6fb095d7272ca7af7ed258b8152d8ec
Robert C <robertryan.chavez@hotmail.com>
2020-05-12 23:23:59 +0200

    ex3_6: wrap methods to a class

--- serie8/ex3_6.py
+++ serie8/ex3_6.py
@@ -1,36 +1,38 @@
 ﻿import numpy as np
 
-
-def tensor1(A, B, display=False):
+class TensorMethods:
     """
-    Calculates the product of two matrices using the first method (see HW Problem 3.6).
-
-    Using numpy's einsum() function it calculates the product with the given axis. 
+    Implements two methods to calculate tensors from two matrices
     """
-    mat = np.einsum('irl,lrj->ij', A, B)
-    if display:
-        print(mat)
-    return mat
+    def tensor1(A, B, display=False):
+        """
+        Calculates the product of two matrices using the first method (see HW Problem 3.6).
 
+        Using numpy's einsum() function it calculates the product with the given axis. 
+        """
+        mat = np.einsum('irl,lrj->ij', A, B)
+        if display:
+            print(mat)
+        return mat
 
-def tensor2(A, B, display=False):
-    """
-    Calculates the product of two matrices using the second method (see HW Problem 3.6).
+    def tensor2(A, B, display=False):
+        """
+        Calculates the product of two matrices using the second method (see HW Problem 3.6).
 
-    Applying the product on the second axis of method 1 will give us our 1-D Array.
-    """
-    mat = np.product(tensor1(A, B), axis=1)
-    if display:
-        print(mat)
-    return mat
+        Applying the product on the second axis of method 1 will give us our 1-D Array.
+        """
+        mat = np.product(TensorMethods.tensor1(A, B), axis=1)
+        if display:
+            print(mat)
+        return mat
 
 
 def main():
     # Test functions
     A = np.array([[[1,2], [3,4]], [[1,2], [3,4]]])
     B = np.array([[[1,2,3], [4,5,6]], [[1,2,3], [4,5,6]]])
-    tensor1(A, B, True)
-    tensor2(A, B, True)
+    TensorMethods.tensor1(A, B, True)
+    TensorMethods.tensor2(A, B, True)
 
 
 if __name__ == "__main__":
