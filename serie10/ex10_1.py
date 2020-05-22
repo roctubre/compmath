d128da6e1270d99dcee7c5cad361e290765d7736
Robert C <robertryan.chavez@hotmail.com>
2020-05-22 06:50:45 +0200

    add ex10.1, ex10.2

---
+++ serie10/ex10_1.py
@@ -0,0 +1,40 @@
+﻿import matplotlib.pyplot as plt
+import numpy as np
+
+
+def main():
+    # create figure
+    fig = plt.figure()
+
+    # b) add two axes
+    ax1 = fig.add_axes([0.11, 0.11, 0.35, 0.8])
+    ax2 = fig.add_axes([0.6, 0.11, 0.35, 0.8])
+     
+    # c) plot, set labels and titles 
+    ax1.set_title("first plot")
+    ax2.set_title("second plot")
+    x = np.linspace(0,2)
+    ax1.set_xlabel("x")
+    ax1.set_ylabel("$y=x^2$")
+    ax2.set_xlabel("x")
+    ax2.set_ylabel("y=x")
+    ax1.plot(x, x**2, color="r", label='$x^2$')
+    ax2.plot(x, x, color="b", label='x')
+    ax1.legend()
+    ax2.legend()
+
+    plt .show()
+
+    # d) What do plt.gca and plt.gcf do?
+    """
+    plt.gca ... get current axes
+    - Returns a handle to the current active axis
+
+    plt.gcf ... get current figure
+    - returns a handle to the current figure
+    - If no current figure exists, a new one is created using figure()
+    """
+
+
+if __name__ == "__main__":
+    main()
---
+++ serie10/ex10_2.py
@@ -0,0 +1,28 @@
+﻿import matplotlib.pyplot as plt
+import numpy as np
+
+
+def main():
+    fig = plt.figure()
+
+    # add big axes
+    ax1 = fig.add_axes([0.09, 0.1, 0.8, 0.8])
+    x = np.linspace(0,100)
+    ax1.set_xlabel("x")
+    ax1.set_ylabel("$x^2$")
+    ax1.plot(x, 2*x, color="r", label='$x^2$')
+    ax1.legend()
+
+    # add smaller axes
+    ax2 = fig.add_axes([0.163, 0.49, 0.2, 0.2])
+    x = np.linspace(20,22)
+    ax2.set_xlabel("x")
+    ax2.set_ylabel("$x^2$")
+    ax2.set_title("zoom")
+    ax2.set_ylim(30,50)
+    ax2.plot(x, 2*x, color="r", label='$x^2$')
+
+    plt .show()
+
+if __name__ == "__main__":
+    main()
